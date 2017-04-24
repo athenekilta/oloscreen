cd257346e5cdb63e6f0a1a7032177c78a185dd1c
KAARLO KOCK <kaarlo.kock@aalto.fi>
2017-04-24 18:16:13 +0300

    Tein vähä juttuja

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['./src/index.js', './src/scss/main.scss'],
+  output: {
+    filename: './dist/assets/js/bundle.js'
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+          use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+    ]},
+    plugins: [
+      new ExtractTextPlugin({ // define where to save the file
+        filename: './dist/assets/css/[name].bundle.css',
+        allChunks: true,
+      }),
+      new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, '/dist'),
+      compress: true,
+      port: 9000,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      }
+    }
+}
