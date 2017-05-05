ddb72a2db8cceaa7701a059523a9799625aa5312
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-05-05 12:18:55 +0300

    värimuutos tehty Niklaksen ehdotuksen mukaisesti.

--- src/js/countDown.js
+++ src/js/countDown.js
@@ -7,13 +7,13 @@ const eventArray = [
   },
   {
     eventName: 'Marathon',
-    date : 'Oct 19, 2018 00:00:01',
-    textEnding: ' bussiin',
+    date : 'Oct 19, 2017 00:00:01',
+    textEnding: ' MARATHONIIN',
     expiredText: 'Bussi kulkee!',
   },
 ];
 
-const countDownDate = new Date("May 1, 2018 00:00:01").getTime();
+const countDownDate = new Date(eventArray[1].date).getTime();
 const x = setInterval(() => {
   const now = new Date().getTime();
   const distance = countDownDate - now;
@@ -23,11 +23,11 @@ const x = setInterval(() => {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("eventHeader").innerHTML = `AIKAA ${eventArray[0].textEnding}`;
+  document.getElementById("eventHeader").innerHTML = `AIKAA ${eventArray[1].textEnding}`;
   document.getElementById("eventTime").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("EVENT").innerHTML = `${eventArray[0].expiredText}`;
+    document.getElementById("EVENT").innerHTML = `${eventArray[1].expiredText}`;
   }
 }, 1000);
